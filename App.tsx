5923ee1c0e5462cfe8d84029ca2144c5dd453122
piyushXP0067 <102165523+piyush3813@users.noreply.github.com>
2024-07-22 15:27:53 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,118 +1,125 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useState } from 'react';
+import { Switch, Text, TouchableOpacity, View } from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
+  const [result, setResult] = useState('');
+  const colors = {
+    dark: '#22252D',
+    dark1: '#292B36',
+    dark2: '#272B33',
+    light: '#FFF',
+    light1: 'rgb(220, 220, 220)',
+    light2: '#F7F7F7',
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+  const getcolor = (light, dark) => darkTheme ? dark : light;
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  const calculate = (title ) => {
+    if (title == 'C') {
+      setResult('')
+    } else if (title == 'DL') {
+      setResult(result.substring(0, result.length - 1));
+    } else if (title == '=') {
+      const ans = Number(eval(result).toFixed(3)).toString();
+      setResult(ans);
+    } else {
+      setResult(result + title);
+    }
+  }
+
+  const Btn = ({ title, type }) => (
+    <TouchableOpacity
+      onPress={() => calculate(title)}
+      style={{
+        padding: 10,
+        borderRadius: 10,
+        elevation: 2,
+        height: 60,
+        width: 60,
+        margin: 16,
+        backgroundColor: getcolor(colors.light, colors.dark)
+      }}>
+      <Text style={{
+        fontSize: 33,
+        // color:'black',
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        color: getBtnColor(type)
+      }}>
+        {title}
+      </Text>
+
+    </TouchableOpacity>
+  );
+  const getBtnColor = (type) => {
+    if (type == 'top') {
+      return '#35FBD6'
+    }
+    else if (type == 'right') {
+
+      return '#EB6363'
+    } else {
+      return getcolor(colors.dark, colors.light)
+    }
+
+  }
+
+
+  return (
+    <View style={{
+      height: '100%',
+      width: '100%',
+      paddingVertical: 20,
+      backgroundColor: getcolor(colors.light, colors.dark),
+      alignItems: 'center'
+    }}>
+      <Switch value={darkTheme}
+        onValueChange={() => setDarkTheme(!darkTheme)}
+        thumbColor={getcolor(colors.dark, colors.light)}
+        trackColor={{ true: colors.light2, false: colors.dark2 }}>
+      </Switch>
+      <Text style={{
+        fontSize: 40,
+        color: getcolor(colors.dark, colors.light),
+        textAlign: 'right',
+        width: '100%',
+        marginTop: 110,
+        paddingRight: 20
+      }}>{result}
+      </Text>
+      <View style={{
+        flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center',
+        backgroundColor: getcolor(colors.light1, colors.dark1),
+        borderTopRightRadius: 20, borderTopLeftRadius: 20
+      }}>
+        <Btn title="C" type='top' />
+        <Btn title="DL" type='top' />
+        <Btn title="/" type='top' />
+        <Btn title="%" type='top' />
+        <Btn title="7" type='number' />
+        <Btn title="8" type='number' />
+        <Btn title="9" type='number' />
+        <Btn title="*" type='right' />
+        <Btn title="4" type='number' />
+        <Btn title="5" type='number' />
+        <Btn title="6" type='number' />
+        <Btn title="-" type='right' />
+        <Btn title="1" type='number' />
+        <Btn title="2" type='number' />
+        <Btn title="3" type='number' />
+        <Btn title="+" type='right' />
+        <Btn title="00" type='number' />
+        <Btn title="0" type='number' />
+        <Btn title="." type='number' />
+        <Btn title="=" type='right' />
+      </View>
+    </View>
+  );
+
+};
+
 
 export default App;
